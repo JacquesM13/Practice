7f4a6ad79c692cd01568ccc8a0f9f6b943c500ae
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2025-02-16 17:42:58 +0000

    Add files via upload

---
+++ Python/HigherOrLower/main.py
@@ -0,0 +1,38 @@
+import game_data
+import random
+import art
+import os
+
+print(art.logo)
+
+wrong = False
+score = 0
+clear = lambda: os.system('clear')
+
+def format_data(data):
+    return f"{data['name']}, a {data['description']}, from {data['country']}"
+
+def check_answer(user_guess, compareA, compareB):
+    if compareA['follower_count'] > compareB['follower_count']:
+        return user_guess == 'a'
+    else:
+        return user_guess == 'b'
+
+while not wrong:
+    compareA = random.choice(game_data.data)
+    compareB = random.choice(game_data.data)
+    if compareB == compareA:
+        compareB = random.choice(game_data.data)
+    print(f"Compare A: {format_data(compareA)}.")
+    print(art.vs)
+    print(f"Against B: {format_data(compareB)}.")
+    if check_answer(input("Who has more followers, A or B? ").lower(), compareA, compareB):
+        score +=1
+        print("\n" * 30)
+        print(art.logo)
+        print(f"You're right! Current score {score}")
+    else:
+        wrong = True
+print("\n" * 30)
+print(art.logo)
+print(f"Sorry that's wrong, final score: {score}")
