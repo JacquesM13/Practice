e3d9ac973d4b4d2c14a5f282118902e4ed51313e
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2025-06-17 14:54:02 +0100

    Create main.py

---
+++ Python/Selenium/Instagram Follower Bot/main.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+USERNAME = ''
+PASSWORD = ''
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options = chrome_options)
+
+# driver.get('https://www.instagram.com/')
+driver.get('https://www.instagram.com/newyorkcity.explore/')
+
+wait = WebDriverWait(driver, 10)
+
+sleep(3)
+
+allow_cookies_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Allow all cookies']")))
+allow_cookies_button.click()
+
+# allow_cookies = driver.find_element(By.XPATH, "//button[text()='Allow all cookies']")
+# allow_cookies.click()
+
+sleep(3)
+
+close_btn = wait.until(
+    EC.element_to_be_clickable((By.XPATH,
+        "//svg[@title='Close' or @aria-label='Close']/ancestor::div[@role='button']"))
+)
+close_btn.click()
+
+# log_in_button = driver.find_element(By.XPATH, "//div[@role='button' and text()='Log in']")
+# log_in_button.click()
+#
+sleep(2)
+
+username_field = driver.find_element(By.NAME, value= 'username')
+username_field.send_keys(USERNAME)
+
+password_field = driver.find_element(By.NAME, value= 'password')
+password_field.send_keys(PASSWORD)
+
+log_in = driver.find_element(By.CSS_SELECTOR, value= '[type="submit"]')
+log_in.click()
